434efb60ae6bbc86d578a087c26994e812f3527a
cecesolihin <49848882+cecesolihin@users.noreply.github.com>
2023-04-03 11:23:41 +0700

    Crud Product

---
+++ DapperMVC/DapperMVC/Controllers/ProductController.cs
@@ -0,0 +1,111 @@
+﻿using DapperMVC.Models;
+using DapperMVC.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DapperMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ProductRepository repo = new ProductRepository();
+        //: Product
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        public ActionResult Search(Product product)
+        {
+            IList<Product> listData = new List<Product>();
+            try
+            {
+                listData = repo.ProductsSearch(product);
+                ViewData["listProduct"] = listData;
+            }
+            catch (Exception ex)
+            {
+
+                Json ("Error : "+ex.Message.ToString());
+            }
+           
+            return PartialView("_GridView");
+        }
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductCreate
+        [HttpPost]
+        public JsonResult Create(Product product)
+        {
+            string result = string.Empty;
+            try
+            {
+
+                repo.AddProduct(product);
+                result = "success";
+            }
+            catch(Exception ex)
+            {
+                result = "Error : " + ex.Message.ToString();
+            }
+            return Json(result,JsonRequestBehavior.AllowGet);
+        }
+
+       
+        // POST: Product/Edit/5
+        [HttpPost]
+        public JsonResult Update(Product product)
+        {
+            string result = string.Empty;
+            try
+            {
+
+                repo.UpdateProduct(product);
+                result = "success";
+            }
+            catch (Exception ex)
+            {
+                result = "Error : " + ex.Message.ToString();
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ProductById(string productId)
+        {
+            string result = string.Empty;
+            Product dataProduct = new Product();
+            try
+            {
+                dataProduct = repo.ProductById(productId); 
+            }
+            catch (Exception ex)
+            {
+                 return Json("Error : " + ex.Message.ToString());
+            }
+            return Json(dataProduct, JsonRequestBehavior.AllowGet);
+        }
+
+        // POST: Product/Delete/5
+        public JsonResult DeleteProduct(string productId)
+        {
+            string result = string.Empty;
+            try
+            {
+
+                repo.DeleteProduct(productId);
+                result = "success";
+            }
+            catch (Exception ex)
+            {
+                result = "Error : " + ex.Message.ToString();
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ DapperMVC/DapperMVC/Repositories/ProductRepository.cs
@@ -0,0 +1,98 @@
+﻿using Dapper;
+using DapperMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DapperMVC.Repositories
+{
+    public class ProductRepository
+    {
+        public readonly string connectionString = ConfigurationManager.ConnectionStrings["connection"].ToString();
+        public IList<Product> GetAll()
+        {
+            IList<Product> listdata = new List<Product>();
+            try
+            {
+                var sql = @"SELECT [ProductId]
+                                 ,[ProductName]
+                                 ,[Price]
+                             FROM[LATIHAN_DB].[dbo].[Product]";
+
+                using (SqlConnection db = new SqlConnection(connectionString))
+                {
+                    listdata = db.Query<Product>(sql).ToList();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return listdata;
+        }
+        public IList<Product> ProductsSearch(Product product)
+        {
+            IList<Product> listdata = new List<Product>();
+            try
+            {
+                var sql = "exec [dbo].[Sp_Product_Search] @ProductId, @ProductName";
+                var productId = string.IsNullOrEmpty(product.ProductId) ? "" : product.ProductId;
+                var productName = string.IsNullOrEmpty(product.ProductName) ? "" : product.ProductName;
+                using (SqlConnection db = new SqlConnection(connectionString))
+                {
+                    listdata = db.Query<Product>(sql, new { productId, productName }).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return listdata;
+        }
+        public Product ProductById(string productId)
+        {
+            Product dataProduct = new Product();
+            var sql = "exec [dbo].[Sp_Product_ById] @ProductId";
+            using (SqlConnection db = new SqlConnection(connectionString))
+            {
+                 dataProduct = db.QuerySingle<Product>(sql, new { productId });
+            }
+            return dataProduct;
+        }
+        public void AddProduct(Product product)
+        {
+            var sql = "exec [dbo].[Sp_Product_Insert] @ProductId, @ProductName,@Price";
+            using (SqlConnection db = new SqlConnection(connectionString))
+            {
+                var exec = db.Execute(sql, new {product.ProductId, product.ProductName,product.Price });
+            }
+              
+        }
+        public void UpdateProduct(Product product)
+        {
+            var sql = "exec [dbo].[Sp_Product_Update] @ProductId, @ProductName,@Price";
+            using (SqlConnection db = new SqlConnection(connectionString))
+            {
+                var exec = db.Execute(sql, new { product.ProductId, product.ProductName, product.Price });
+            }
+
+        }
+        public void DeleteProduct(string productId)
+        {
+            var sql = "exec [dbo].[Sp_Product_Delete] @ProductId";
+            using (SqlConnection db = new SqlConnection(connectionString))
+            {
+                var exec = db.Execute(sql, new { productId});
+            }
+
+        }
+    }
+}
